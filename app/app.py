8bf5156008e2122e21e3787d0e66b2c1a8a500cf
Kirill Mineev <mineev@rtf17.ru>
2020-06-07 23:29:46 +0500

    Dsa

--- app/app.py
+++ app/app.py
@@ -52,7 +52,7 @@ async def cors_middleware(app: web.Application, handler):
 
 async def init_app():
     app = web.Application(middlewares=[cors_middleware])
-    red = await aioredis.create_redis_pool('redis://0.0.0.0:6379/10')
+    red = await aioredis.create_redis_pool('redis://45.86.182.252:6379/10')
 
     app['redis'] = red
 
